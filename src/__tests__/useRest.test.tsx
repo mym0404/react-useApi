dea8ed9c2fe656842dd4be62e0965dca1572ca83
myeongju Mun <mym0404@gmail.com>
2020-04-06 10:33:30 +0900

    create test cases

--- src/__tests__/useRest.test.tsx
+++ src/__tests__/useRest.test.tsx
@@ -1,54 +1,115 @@
-import { ApiResult } from '..';
+import { ApiResult, useRest } from '..';
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { useCall } from '../useRest';
 
 type ResponseType = {
   name: string;
   age: number;
 };
-const responseData: ResponseType = { name: 'dooboo', age: 24 };
-let mockUnsubscribe = jest.fn();
-let mockCall = jest.fn();
-let apiResult: ApiResult<ResponseType>;
+const responseData: ResponseType = { name: 'mj', age: 24 };
+let unsubscribeMock = jest.fn();
+let dataPromiseMock = jest.fn();
+let apiResultMock: ApiResult<ResponseType>;
+
+let dependencies: any[] = [1, 2, 3];
 
 describe('useRest', () => {
   beforeEach(() => {
-    apiResult = [mockCall, mockUnsubscribe];
-    mockUnsubscribe.mockReset();
-    mockCall.mockReset();
-    mockCall.mockResolvedValueOnce(responseData);
+    apiResultMock = [dataPromiseMock, unsubscribeMock];
+    unsubscribeMock.mockReset();
+    dataPromiseMock.mockReset();
+    dataPromiseMock.mockResolvedValueOnce(responseData);
   });
-  it('', () => {});
 
-  // it('render success', async () => {
-  //   const { result, waitForNextUpdate } = renderHook(() => useRest(apiResult, [], true));
-  //   await waitForNextUpdate();
-  //   expect(result.current).toBeTruthy();
-  // });
-  //
-  // it('hot api call should be success and states have right values', async () => {
-  //   const {
-  //     result: {
-  //       current: { name, age, call, error, loading, success, unsubscribe },
-  //     },
-  //     waitForNextUpdate,
-  //     waitForValueToChange,
-  //   } = renderHook(() => useRest(apiResult, []));
-  //
-  //   expect(name).toBeUndefined();
-  //   expect(age).toBeUndefined();
-  //
-  //   expect(call).toBeNull();
-  //   expect(error).toBeNull();
-  //   expect(loading).toBe(true);
-  //   expect(success).toBe(false);
-  //   expect(unsubscribe).toBeTruthy();
-  //
-  //   while (true) {
-  //     console.log(name, age, call, error, loading, success, unsubscribe);
-  //     await waitForNextUpdate();
-  //     console.log(name, age, call, error, loading, success, unsubscribe);
-  //   }
-  //
-  //   expect(name).toBe('dooboo');
-  //   expect(age).toBe(24);
-  // });
+  it('hot api call should change states corretly', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useRest(apiResultMock, []));
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.name).toBe(undefined);
+
+    await waitForNextUpdate();
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.name).toBe('mj');
+
+    expect(dataPromiseMock).toBeCalledTimes(1);
+  });
+
+  it('cold api call should change states corretly', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useCall(apiResultMock, []));
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.name).toBe(undefined);
+
+    act(() => {
+      result.current.call();
+    });
+    expect(result.current.loading).toBe(true);
+
+    await waitForNextUpdate();
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.name).toBe('mj');
+
+    expect(dataPromiseMock).toBeCalledTimes(1);
+  });
+
+  it('Api call failed', async () => {
+    const failedApi = jest.fn().mockRejectedValue(new Error('api is failed'));
+    const apiResultMock: ApiResult = [failedApi, unsubscribeMock];
+
+    const { result, waitForNextUpdate } = renderHook(() => useRest(apiResultMock));
+
+    await waitForNextUpdate();
+
+    expect(failedApi).toBeCalledTimes(1);
+  });
+
+  it('dependencies will be dirty', async () => {
+    const { rerender, waitForNextUpdate } = renderHook(() => useRest(apiResultMock, dependencies));
+
+    await waitForNextUpdate();
+    expect(dataPromiseMock).toBeCalledTimes(1);
+
+    dependencies = [4, 5, 6];
+    rerender();
+
+    await waitForNextUpdate();
+    expect(dataPromiseMock).toBeCalledTimes(2);
+
+    dependencies = [1];
+    rerender();
+
+    await waitForNextUpdate();
+    expect(dataPromiseMock).toBeCalledTimes(3);
+
+    dependencies = ['string'];
+    rerender();
+
+    await waitForNextUpdate();
+    expect(dataPromiseMock).toBeCalledTimes(4);
+
+    // If dependencies is same, then call will be not invoked/
+
+    dependencies = ['string'];
+    rerender();
+
+    expect(dataPromiseMock).toBeCalledTimes(4);
+  });
+
+  it('unmount will unsubscribe', async () => {
+    const { rerender, waitForNextUpdate, unmount } = renderHook(() => useRest(apiResultMock));
+    unmount();
+  });
+
+  it('when api throw,  unmount will unsubscribe', async () => {
+    const failedApi = jest.fn().mockRejectedValue(new Error('api is failed'));
+    const apiResultMock: ApiResult = [failedApi, unsubscribeMock];
+
+    const { rerender, waitForNextUpdate, unmount } = renderHook(() => useRest(apiResultMock));
+    unmount();
+  });
 });
